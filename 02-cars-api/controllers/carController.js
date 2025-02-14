3f9a8c8acaf388a8964e9fa709b4d9cc2c9de1a9
César Guerra <warderer@gmail.com>
2025-02-13 19:08:06 -0600

    ✨ getAllCars and getCarById endpoints

--- 02-cars-api/controllers/carController.js
+++ 02-cars-api/controllers/carController.js
@@ -11,11 +11,31 @@ const createCar = async (req, res) => {
 }
 
 // READ
+const getAllCars = async (req, res) => {
+  try {
+    const cars = await Car.find()
+    // const cars = await Car.find({}, { plate: 1, year: 1, model: 1, brand: 1 }) // Con proyecciones, para elegir solamente ciertos campos.
+    res.status(200).json(cars)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+const getCarById = async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.carId)
+    res.status(200).json(car)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
 
 // UPDATE
 
 // DELETE
 
 export {
-  createCar
+  createCar,
+  getAllCars,
+  getCarById
 }
