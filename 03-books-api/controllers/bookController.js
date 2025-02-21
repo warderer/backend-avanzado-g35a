a01b329483bd668117eda751994faa99c8b36245
César Guerra <warderer@gmail.com>
2025-02-20 19:12:23 -0600

    ✨ Add Authors info with Populate to getAllBooks endpoint

--- 03-books-api/controllers/bookController.js
+++ 03-books-api/controllers/bookController.js
@@ -50,7 +50,7 @@ const createBook = async (req, res) => {
 // READ
 const getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find({ isActive: true })
+    const books = await Book.find({ isActive: true }).populate('authors', 'firstName lastName bio birthDate')
     if (!books) {
       return res.status(404).json({ message: 'No se encontraron libros' })
     }
